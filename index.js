1a06c497565b41ab00beb47d46d11ea405b1c628
dhr231 <dhruvprashar231@gmail.com>
2025-11-09 01:23:46 +0530

    Added - Form Implementation for Bookings

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/bookings', (req,res) => {
-    // res.send("Server up and running");
-    const bookings = [
-        {
-            id:1,
-            customer: "Rahul",
-            service: "Logistics",
-            date: "2025-11-08",
-            amount: 1500
-        },
-        {
-            id:2,
-            customer: "Sneha",
-            service: "Transport",
-            date: "2025-11-09",
-            amount: 2000
-        }
-    ];
-    res.json(bookings);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+bookings = [];
+app.post('/bookings', (req, res) => {
+    const { id, customer, service, date, mode, amount } = req.body;
+    if (!id || !customer || !service || !date || !mode || !amount) {
+        return res.status(400).json({ error: "All fields are required." });
+    }
+    const newBooking = { id, customer, service, date, mode, amount };
+    bookings.push(newBooking);
+    return res.status(201).json(newBooking);
 })
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
